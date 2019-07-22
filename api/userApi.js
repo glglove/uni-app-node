124c5a75c8981db3f7a85fa44e9cf910c6cb3104
gaol <331319344@qq.com>
2019-07-22 18:38:13 +0800

    update

--- api/userApi.js
+++ api/userApi.js
@@ -7,12 +7,11 @@ const userApi= {
   // 用户注册
   register: function (params) {
     return new Promise((resolve, reject) => {
-      console.log(params)
       return Users.findOne({name: params.name}).then((data) => {
         // console.log(data)
         if (data) {
           resolve({
-            state: 1,
+            code: 1,
             msg: "用户名已存在",
           })
         } else {
@@ -20,23 +19,6 @@ const userApi= {
             name: params.name,
             pwd: params.pwd
           }
-
-          // const data = {
-          //   Method: 'GetFlowList',
-          //   TokenId: '141B00F6-FD61-4BAD-A39D-9D89B779FA5F',
-          //   CompanyCode: 05255594,
-          //   UserId: 3194
-          // }
-          // // // 向 java 后台服务器发送 请求
-          // http.post('https://www.caihuiyun.cn/WorkFlow', JSON.stringify(data)).then(res => {
-          //   if (res) {
-          //     console.log(1112)
-          //     res.send(254)
-          //   } else {
-          //     res.send(423454)
-          //     console.log(232)
-          //   }
-          // })
 
           // 密码进行加密后返回
           // const saltRounds = 10;
@@ -46,18 +28,21 @@ const userApi= {
           // 实例化 myUser
           const newUser = new Users(myUser)
           console.log(newUser)
-          // 存储 
+          // 存储到mongodb中
           newUser.save().then(user => {
-            resolve({
-              state: 1,
-              msg: '用户信息存储成功',
-              user
-            })
+            if(user){
+              resolve({
+                state: 1,
+                msg: '用户信息存储成功',
+                user
+              })
+            }else {
+              reject("存入mongodb失败！")
+            }
           }).catch(err => {
             console.log(err)
             reject(err)
           })
-
         }
       }).catch(err => {
         console.log(err)
