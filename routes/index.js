a534cec5550b4d82444fa720140e1d5dc461611f
gaol <331319344@qq.com>
2019-11-27 08:53:21 +0800

    update1127

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,15 @@ var path = require('path')
 // console.log(". = %s", path.resolve("."));
 console.log("__dirname = %s", path.resolve(__dirname));
 
+
+router.get('/index', (req, res, next) => {
+  // res.send("欢迎来到 express 的世界")
+  // res.sendFile('../pages/index.html')
+  // console.log(path.resolve(__dirname, '../pages') + '/index.html');
+  res.sendFile(path.resolve(__dirname, '../pages') + '/index.html');
+  next()
+})
+
 /* GET home page. */
 router.get('*', function(req, res, next) {
   // res.render('index', { title: 'Express' });  // 发送的是 ejs 模板引擎  views 中的 index.ejs 文件内容
@@ -17,16 +26,9 @@ router.get('*', function(req, res, next) {
   // res.send('<p>some html</p>');
   // res.status(404).send('Sorry, we cannot find that!');
   // res.status(500).send({ error: 'something blew up' });
-  // res.sendFile('../pages/index.html')
-    // console.log("---------------------------------",path.resolve(__dirname));
-    res.sendFile(path.resolve(__dirname, '../dist') + '/index.html');
-});
-
-router.get('/index', (req, res, next) => {
-  // res.send("欢迎来到 express 的世界")
-  // res.sendFile('../pages/index.html')
-  // console.log(path.resolve(__dirname, '../pages') + '/index.html');
   res.sendFile(path.resolve(__dirname, '../pages') + '/index.html');
-})
+  // console.log("---------------------------------",path.resolve(__dirname));
+  // res.sendFile(path.resolve(__dirname, '../dist') + '/index.html');
+});
 
 module.exports = router; 
