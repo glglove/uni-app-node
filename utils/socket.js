d22ef85cb1d8ebb36283feb1650f73557cd3551a
331319344@qq.com <331319344@qq.com>
2019-12-18 22:25:31 +0800

    update

--- utils/socket.js
+++ utils/socket.js
@@ -3,10 +3,7 @@ var _ = require('underscore');
 module.exports =  {
     socketUrl: '127.0.0.1:5000',
     hashName: new Array(),
-    connect: function(server){
-        // 引入 socket.io 
-        var io = require('socket.io').listen(server)
-  
+    connect: function(io){
         //监听客户端链接,回调函数会传递本次链接的socket
         io.on('connection', socket => {
             console.log("连接成功后node服务端打印连接成功后的socket.id-----",socket.id)
@@ -20,7 +17,6 @@ module.exports =  {
 
             // 连接成功后向客户端发送
             io.emit("connectionSuccess","这是node服务器发送过来的消息:socket连接成功！")
-
             // 监听 客户端发过来的 bindId 事件
             socket.on("bindId", (data) => {
                 let userId = data.userId
@@ -30,6 +26,21 @@ module.exports =  {
                 console.log("服务端 接收客户端 发送来的 bindId 事件", JSON.stringify(data))
             })
             
+            // var toName = data.to_Name;
+            // // 发送的目标客户的token
+            // var toToken = data.to_Token;
+
+            // if (toToken = this.hashName[toName]) {
+            //     // nodejs的underscore扩展中的findWhere方法，可以在对象集合中，通过对象的属性值找到该对象并返回。
+            //     var toSocket = _.findWhere(io.sockets.sockets, {id: toId});
+        
+            //     // socket.emit() ：向建立该连接的客户端广播
+            //     // socket.broadcast.emit() ：向除去建立该连接的客户端的所有客户端广播
+            //     // io.sockets.emit() ：向所有客户端广播，等同于上面两个的和
+        
+            //     // 通过该连接对象（toSocket）与链接到这个对象的客户端进行单独通信   
+            // }       
+        
             socket.on("testMsg", (data) => {
             // 监听客户端发送过来的 testMsg 事件
                 console.log("客户端发送过来的testMsg事件-------", JSON.stringify(data))
@@ -38,8 +49,6 @@ module.exports =  {
                 var to_Token = data.to_Token
                 var from_Id = data.from_Id
                 var from_Token = data.from_Token
-                var to_url = data.to_url
-                var to_url_type = data.to_url_type
                 // this.hashName[data.from_Id] = socket.id
                 console.log("----------------",this.hashName[data.from_Id])
                 if(this.hashName[from_Id]){
@@ -47,11 +56,9 @@ module.exports =  {
                     // 通过_.findWhere() 方法来找到 指定的这个socket 对象
                     var fromSocketObj = _.findWhere(io.sockets.sockets, {id: this.hashName[data.from_Id]})
                     // 向指定的某一个客户端发送消息
-                    fromSocketObj.emit("emitFixedCustomerInfo",{
+                    fromSocketObj.emit("emitFixed",{
                         from_id: from_Id,
-                        from_msg: msg,
-                        to_url: to_url,
-                        to_url_type: to_url_type
+                        from_msg: msg
                     })
                     // 向除了指定的客户端以外的用户发送消息
                     // fromSocketObj.broadcast.emit("emitFixed", {
@@ -70,7 +77,6 @@ module.exports =  {
                 }
 
             })
-            
             // 监听连接断开事件
             socket.on("disconnect", () => {
                 console.log("连接已断开...");
